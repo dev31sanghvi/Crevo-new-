6c582b87ee68a57822645c7105a7f154a0f82b44
Dev Sanghvi <105737978+dev31sanghvi@users.noreply.github.com>
2024-04-21 03:45:45 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ import AboutUs from '../pages/about';
 import Contact from '../pages/contact';
 import { IconHome, IconPhone, IconUsers } from "@tabler/icons-react";
 import { BackgroundBeams } from "../pages/background";
-
+  import {GridBackgroundDemo} from "../components/Hero";
 function App() {
   const navItems = [
     {
@@ -31,6 +31,7 @@ function App() {
   return (
     <>
     <BackgroundBeams />
+    <GridBackgroundDemo />
       <Router>
       <div className="relative  w-full">
         <FloatingNav navItems={navItems}/>
